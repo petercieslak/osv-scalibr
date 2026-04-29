28ae14b8e5b7e20e2d3fa6355edb4b6f2835ecdc
Yuvraj Saxena <ysaxenax@gmail.com>
2026-04-29 22:35:19 +0530

    Annotator: filter embedded FS packages for plugins with the RunningSystem capability
    Annotators that require a running system may execute commands or binaries
    on the target filesystem. However, packages originating from embedded
    filesystems are extracted artifacts and do not represent a live system.
    This can lead to incorrect behavior, especially when binaries belong to a
    different architecture and emulation is not supported.
    
    To address this, filter out packages originating from embedded
    filesystems before passing the inventory to annotators with
    RunningSystem requirements. Annotators that do not require a running
    system continue to receive the full inventory.
    
    Closes: https://github.com/google/osv-scalibr/issues/2008

--- annotator/annotator.go
+++ annotator/annotator.go
@@ -17,7 +17,10 @@ package annotator
 
 import (
 	"context"
+	"os"
+	"strings"
 
+	"github.com/google/osv-scalibr/extractor"
 	scalibrfs "github.com/google/osv-scalibr/fs"
 	"github.com/google/osv-scalibr/inventory"
 	"github.com/google/osv-scalibr/plugin"
@@ -57,9 +60,67 @@ func Run(ctx context.Context, config *Config, inventory *inventory.Inventory) ([
 		ScanRoot: config.ScanRoot,
 	}
 
+	// This is required to prevent passing packages from embedded filesystems to
+	// plugins that issue commands on packages.
+	//
+	// Some annotator plugins require the system to be in a running state to perform
+	// tasks such as executing commands or running binaries on the target filesystem.
+	// Embedded filesystems, however, are not mounted as live systems; they are
+	// extracted to the local filesystem for analysis by Scalibr.
+	//
+	// This is particularly important when the embedded filesystem contains binaries
+	// for an architecture different from the one Scalibr is currently running on,
+	// since emulation is not currently supported.
+	//
+	// Therefore, packages originating from embedded filesystems are filtered out and
+	// not supplied to plugins that require a running system.
+	filteredInventory := filterOutEmbeddedPackages(inventory)
+
 	for _, a := range config.Annotators {
-		err := a.Annotate(ctx, input, inventory)
+		var err error
+
+		if !a.Requirements().RunningSystem {
+			err = a.Annotate(ctx, input, inventory)
+		} else {
+			err = a.Annotate(ctx, input, filteredInventory)
+		}
+
 		statuses = append(statuses, plugin.StatusFromErr(a, false, err, nil))
 	}
 	return statuses, nil
+}
+
+func filterOutEmbeddedPackages(inv *inventory.Inventory) *inventory.Inventory {
+	if inv == nil {
+		return &inventory.Inventory{}
+	}
+	filtered := *inv // shallow copy
+
+	var pkgs []*extractor.Package
+	for _, p := range inv.Packages {
+		if !isPackageFromEmbeddedFS(p) {
+			pkgs = append(pkgs, p)
+		}
+	}
+
+	filtered.Packages = pkgs
+	return &filtered
+}
+
+// isPackageFromEmbeddedFS returns true if the package originates from an embedded filesystem.
+func isPackageFromEmbeddedFS(pkg *extractor.Package) bool {
+	location := pkg.Location.PathOrEmpty()
+	if location == "" {
+		return false
+	}
+
+	parts := strings.Split(location, ":")
+
+	// Windows: skip drive letter (e.g., "C:\")
+	if os.PathSeparator == '\\' {
+		return len(parts) >= 3
+	}
+
+	// Linux/macOS: embedded FS typically has at least one ":" separator
+	return len(parts) >= 2
 }
